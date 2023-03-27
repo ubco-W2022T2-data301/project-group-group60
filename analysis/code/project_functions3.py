bf40a683a6e8c35268c2c64a0f3fc279d3bdca4d
Ojus Sharma <ojus@student.ubc.ca>
2023-03-27 13:21:59 -0700

    Changes

---
+++ analysis/code/project_functions3.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+def load_and_process(url_or_path_to_csv_file):
+
+    # Method Chain 1 (Load data and deal with missing data)
+
+    df1 = (
+          pd.read_csv(url_or_path_to_csv_file)
+          .rename(...)
+          .dropna(...)
+      )
+
+    # Method Chain 2 (Create new columns, drop others, and do processing)
+
+    df2 = (
+          df1
+          .assign(...)
+      )
+
+    # Make sure to return the latest dataframe
+
+    return df2 
+
+
+def find_unique_wicketkeepers(df):
+    df1 = (
+            (df.loc[:, ['ID', 'kind', 'fielders_involved']]
+            [(df['kind'] == 'stumped')])
+            ['fielders_involved'].unique()
+    )
+    
+    return df1
