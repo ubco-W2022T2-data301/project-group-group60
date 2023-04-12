93bdb8b8de63971ae89929bd8a4647d8d9e185f4
Ojus Sharma <ojus@student.ubc.ca>
2023-04-12 00:25:47 -0700

    Updating my analysis

--- analysis/codes/project_functions3.py
+++ analysis/codes/project_functions3.py
@@ -17,54 +17,71 @@ def find_unique_wicketkeepers(df):
     return df1
 
 def find_wicketkeeper_stats(bb_df, us):
-    df = bb_df
-    df = df.loc[:, ['ID', 'kind', 'fielders_involved']]
-    keeper_stats = df[(df['fielders_involved'].isin(us))]
-    ks = keeper_stats
-
-    temp = ks[ks['kind'] == 'caught']
-    final_wicketkeeping =  ks.groupby(temp["fielders_involved"])[["kind"]].count()
-
-    temp = ks[ks['kind'] == 'stumped']
-    df2 =  ks.groupby(temp["fielders_involved"])[["kind"]].count()
-
-    df3 = pd.DataFrame(ks.groupby('fielders_involved')['ID'].nunique())
-    final_wicketkeeping['Stumpings'] = df2['kind']
-    final_wicketkeeping['Innings'] = df3['ID']
-    final_wicketkeeping = final_wicketkeeping.rename(columns={'kind': 'Catches'})
-    
-    
-    final_wicketkeeping['Dismissals'] = (final_wicketkeeping['Catches'] + final_wicketkeeping['Stumpings'])
-    final_wicketkeeping['Dis/Inn'] = (final_wicketkeeping['Dismissals'])/final_wicketkeeping['Innings']
-    final_wicketkeeping= final_wicketkeeping.sort_values('Dis/Inn', ascending=False)
-    final_wicketkeeping = final_wicketkeeping.round(2)
+    final_wicketkeeping = (
+        bb_df.loc[:, ['ID', 'kind', 'fielders_involved']]
+        .query("fielders_involved in @us")
+        .assign(
+            Catches=lambda x: x['kind'].eq('caught').astype(int),
+            Stumpings=lambda x: x['kind'].eq('stumped').astype(int)
+            )
+        .groupby('fielders_involved')
+        .agg(
+            Catches=('Catches', 'sum'),
+            Stumpings=('Stumpings', 'sum'),
+            Innings=('ID', 'nunique')
+            )
+        .assign(
+            Dismissals=lambda x: x['Catches'] + x['Stumpings'],
+            Dis_Inn=lambda x: x['Dismissals'] / x['Innings']
+            )
+        .rename(columns={'Catches': 'Catches', 'Stumpings': 'Stumpings', 'Innings': 'Innings', 'Dis_Inn': 'Dis/Inn'})
+        .sort_values('Dis/Inn', ascending=False)
+        .round(2)
+    )
     
     return final_wicketkeeping
 
 
 def find_and_merge_batting_stats_of_keepers(fws):
-    kps = fws.index.tolist()
     btsmn = pd.read_csv('../data/processed/Ishaan/batsman_stats.csv')
+    kps = fws.index.tolist()
+    
+    
     btsmn = btsmn[ btsmn['batter'].isin(kps)]
     fws = fws.reset_index().sort_values(by = 'fielders_involved')
-    btsmn= btsmn.reset_index().sort_values(by = 'batter').rename(columns={'batter':'fielders_involved'})
-    keeper_batsmen = pd.merge(fws, btsmn, on='fielders_involved', how='left').drop(columns=['index','Matches','Total Runs','Balls Faced', 'Wickets']).dropna().round(2).rename(columns={'Runs scored per wicket':'Runs Per Wicket'})
+    
+    btsmn= (
+            btsmn.reset_index()
+            .sort_values(by = 'batter')
+            .rename(columns={'batter':'fielders_involved'})
+        )
+    
+    keeper_batsmen = (
+            pd.merge(fws, btsmn, on='fielders_involved', how='left')
+            .drop(columns=['index','Matches','Total Runs','Balls Faced', 'Wickets'])
+            .dropna()
+            .round(2)
+            .rename(columns={'Runs scored per wicket':'Runs Per Wicket'})
+        )
+        
     return keeper_batsmen
 
 def find_allrounder_stats():
     btsmn = pd.read_csv('../data/processed/Ishaan/batsman_stats.csv')
-    x = btsmn['batter'].unique().tolist()
     bwlr = pd.read_csv('../data/processed/bowlerstatssparsh.csv')
-    bwlr = bwlr[ bwlr['bowler'].isin(x)]
-    btsmn= btsmn.rename(columns={'batter':'bowler'})
-    allr = pd.merge(bwlr, btsmn, on='bowler', how='left').round(2)
-   # allr = all.drop(columns=['index','Matches','Total Runs','Balls Faced', 'Wickets'])
-    #allr.to_csv("../data/processed/allRounder_stats.csv")
-    df = allr
-    df['Runs Given per Wicket'] = df['total_run']/df['wickets_taken']
-    df = df.drop(columns=['Wickets', 'Balls Faced', 'overs_bowled', 'balls_bowled', 'extras_given']).rename(columns={'total_run' : 'Runs Given', 'wickets_taken' : 'Wickets Taken', 'Total Runs' : 'Runs Scored'}).round(2)
+
+    allr = (bwlr[bwlr['bowler'].isin(btsmn['batter'].unique().tolist())]
+        .merge(btsmn.rename(columns={'batter': 'bowler'}), on='bowler', how='left')
+        .assign(Runs_Given_per_Wicket=lambda x: x['total_run'] / x['wickets_taken'],
+                Runs_Given=lambda x: x['total_run'],
+                Wickets_Taken=lambda x: x['wickets_taken'],
+                Runs_Scored=lambda x: x['Total Runs'])
+        .round(2)
+        .drop(columns=['Wickets', 'Balls Faced', 'overs_bowled', 'balls_bowled', 'extras_given', 'total_run', 'wickets_taken'])
+    )
+    
     #df.to_csv("../data/processed/allRounder_stats1.csv")
-    return df
+    return allr
 
 
 
